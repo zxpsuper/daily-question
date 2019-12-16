a69621120b1a16e40ce7a08f30136f00a4080de6
suporka <zxpscau@163.com>
2019-12-16 15:18:57 +0800

    更新

--- test.js
+++ test.js
@@ -1,22 +1,10 @@
-function isPrime(num) {
-  if (typeof num !== 'number') {
-    throw new TypeError('num should be number')
-  }
-  if (num === 2 || num === 3) {
-    return true;
-  };
-  if (num % 2 === 0) {
-    return false;
-  };
-  let divisor = 3, limit = Math.sqrt(num);
-  while (limit >= divisor) {
-    if (num % divisor === 0) {
-      return false;
-    }
-    else {
-      divisor += 2;
-    }
-  }
-  return true;
+function isPalindrome(str) {
+  str = '' + str;
+  return (
+    Array.from(str)
+      .reverse()
+      .join('') === str
+  );
 }
-console.log(isPrime(30));  // false
+
+console.log(isPalindrome('strts'));
