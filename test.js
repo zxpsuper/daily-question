4b1db85e568777aed070f4cd72500908fc5788b8
zxpsuper <zxpscau@163.com>
2020-03-12 12:08:28 +0800

    更新

--- test.js
+++ test.js
@@ -1,57 +1,14 @@
-function Node(val) {
-  this.left = null;
-  this.right = null;
-  this.value = val;
-}
-
-function generateBST(root, array) {
-  var length = array.length;
-
-  for (var i = 1; i < length; i++) {
-    insertNode(root, array[i]);
-  }
-
-}
-
-function insertNode(node, value) {
-  if (value < node.value) {
-    if (node.left === null) {
-      node.left = new Node(value);
-    } else {
-      node = node.left;
-      insertNode(node, value);
-    }
-  } else {
-    if (node.right === null) {
-      node.right = new Node(value);
-    } else {
-      node = node.right;
-      insertNode(node, value);
+function insertSort(arr) {
+  for(let i = 1; i < arr.length; i++) {  //外循环从1开始，默认arr[0]是有序段
+    for(let j = i; j > 0; j--) {  //j = i,将arr[j]依次插入有序段中
+      if(arr[j] < arr[j-1]) {
+        [arr[j],arr[j-1]] = [arr[j-1],arr[j]];
+      } else {
+        break;
+      }
     }
   }
+  return arr;
 }
 
-var array = [2, 3, 4, 12, 3, 54, 6, 7, 1];
-var root = new Node(array[0]);
-
-generateBST(root, array);
-
-// 中序遍历
-function inorderSearch(root) {
-  var array = [];
-
-  _inorderSearch(root, array);
-  return array;
-
-}
-
-function _inorderSearch(node, array) {
-  if (!node) {
-    return;
-  }
-  _inorderSearch(node.left, array);
-  array.push(node.value);
-  _inorderSearch(node.right, array);
-}
-console.log(root);
-console.log(inorderSearch(root));
+console.log(insertSort([3,2,1,9,6,45,7]));
